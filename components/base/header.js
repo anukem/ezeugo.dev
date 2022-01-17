e9f5975d393e125d6df98a33dd47b1256a5b686d
Ezeugo Anukem <jea2161@columbia.edu>
2022-01-16 21:56:07 -0500

    got the header

--- components/base/header.js
+++ components/base/header.js
@@ -1,10 +1,22 @@
-import styles from "./header.module.css";
+import styles from "./header.module.scss";
 import Link from "next/link";
+import { COLORS } from "../constants/constants";
+import { Search } from "../icons/search.js";
 export default function Header({ children }) {
   return (
     <>
       <div className={styles.container}>
-        <Link href={"/"}>Home</Link>
+        <div className={styles.links}>
+          <Link href={"/"}>Home</Link>
+          <Link href={"/"}>Most Popular</Link>
+        </div>
+        <div className={styles.title}>readonly </div>
+        <div
+          onClick={() => console.log("i got clicked")}
+          className={styles.searchContainer}
+        >
+          <Search width={"20px"} height={"20px"} color={COLORS.PAGE_WHITE} />
+        </div>
       </div>
       {children}
     </>
---
+++ components/constants/constants.js
@@ -0,0 +1,4 @@
+export const COLORS = {
+  PAGE_WHITE: "#fafafa",
+  CALM_BLACK: "#434343",
+};
